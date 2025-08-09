757b2adcbfc81876db9096866076cc547d29668d
Min ThantTun <minthanttun.mm@gmail.com>
2025-08-09 13:58:10 +0630

    Revert "Add files via upload"
    This reverts commit 90de2d7de6870ddac31d6985499a39e442e86821.

--- types.js
+++
@@ -1,5 +0,0 @@
-// Type definitions for the SkinCare application - Compiled to JavaScript
-// This file contains type definitions that are used throughout the application
-
-// Export empty object since this is just type definitions
-export {};
